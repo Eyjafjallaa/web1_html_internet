d5c87ce6556db907bb45acd58f4db52a322c4c9d
Eyjafjallaa <ysson51@gmail.com>
2020-09-30 23:26:05 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,20 +2,25 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
+var app = http.createServer(function (request, response) {
+  var _url = request.url;
+  var queryData = url.parse(_url, true).query;
+  var pathname = url.parse(_url, true).pathname;
 
-var app = http.createServer(function(request,response){
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    var pathname = url.parse(_url, true).pathname;
-    var title = queryData.id;
+  if (pathname === '/') {
+    if (queryData.id === undefined) {
+      fs.readdir('./data', function (error, filelist) {
+        var title = 'Welcome';
+        var description = "Hello, Node.js";
 
-    if(pathname==='/'){
-        if(queryData.id===undefined)
-        {   
-            fs.readFile(`data/${queryData.id}`,'utf8',function(err,description){
-            var title=  'Welcome';
-            var description = "Hello, Node.js";
-                var template=`
+        var list = '<ul>';
+        var i = 0;
+        for (i = 0; i < filelist.length; i++) {
+          list += `<li><a 
+              href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+        }
+        list += '</ul>';
+        var template = `
                 <!doctype html>
                 <html>
                 <head>
@@ -24,54 +29,56 @@ var app = http.createServer(function(request,response){
                 </head>
                 <body>
                   <h1><a href="/">WEB</a></h1>
-                  <ul>
-                    <li><a href="/?id=HTML">HTML</a></li>
-                    <li><a href="/?id=CSS">CSS</a></li>
-                    <li><a href="/?id=JavaScript">JavaScript</a></li>
-                  </ul>
+                  ${list}
                   <h2>${title}</h2>
                   <p>${description}</p>
                 </body>
                 </html>    
                 `;
-                response.writeHead(200);
-                response.end(template);
-            });
-        }
-        else
-        {
-            fs.readFile(`data/${queryData.id}`,'utf8',function(err,description){
-                var title = queryData.id;
-                var template=`
-                <!doctype html>
-                <html>
-                <head>
-                  <title>WEB1 - ${title}</title>
-                  <meta charset="utf-8">
-                </head>
-                <body>
-                  <h1><a href="/">WEB</a></h1>
-                  <ul>
-                    <li><a href="/?id=HTML">HTML</a></li>
-                    <li><a href="/?id=CSS">CSS</a></li>
-                    <li><a href="/?id=JavaScript">JavaScript</a></li>
-                  </ul>
-                  <h2>${title}</h2>
-                  <p>${description}</p>
-                </body>
-                </html>    
-                `;
-                response.writeHead(200);
-                response.end(template);
-            });
-        }
+        response.writeHead(200);
+        response.end(template);
+      })
     }
-    else
-    {
-            response.writeHead(404);
-            response.end('Not found');
+    else {
+
+      fs.readdir('./data', function (error, filelist) {
+        var list = '<ul>';
+        var i = 0;
+        for (i = 0; i < filelist.length; i++) {
+          list += `<li><a 
+              href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+        }
+        list += '</ul>';
+
+        fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
+          var title = queryData.id;
+          var template = `
+              <!doctype html>
+              <html>
+              <head>
+                <title>WEB1 - ${title}</title>
+                <meta charset="utf-8">
+              </head>
+              <body>
+                <h1><a href="/">WEB</a></h1>
+              ${list}
+                <h2>${title}</h2>
+                <p>${description}</p>
+              </body>
+              </html>    
+              `;
+          response.writeHead(200);
+          response.end(template);
+
+        });
+      });
     }
-    
+  }
+  else {
+    response.writeHead(404);
+    response.end('Not found');
+  }
+
 
 });
 app.listen(80);
